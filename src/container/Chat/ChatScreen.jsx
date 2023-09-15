165c2bb9d5deadf3d99ab40a75160e0148ca8b7b
shibil <shibil@keyvalue.systems>
2023-09-15 23:27:31 +0530

    update chat history ui

--- src/container/Chat/ChatScreen.jsx
+++ src/container/Chat/ChatScreen.jsx
@@ -1,6 +1,7 @@
 import ChatInput from "./ChatInput";
 import MessageList from "./MessageList.jsx";
 import React, { useState } from "react";
+import "./ChatScreen.scss";
 
 const ChatScreen = ({ setJsonData, setLoading, userId, loading }) => {
   const [message, setMessage] = useState([]);
@@ -10,14 +11,18 @@ const ChatScreen = ({ setJsonData, setLoading, userId, loading }) => {
     else setMessage((prev) => [...prev, msg]);
   };
   return (
-    <div className=" bg-white flex flex-col px-10 pt-10 pb-4 rounded-lg space-y-3 my-4 mx-2 w-full h-full justify-between">
-      <MessageList list={message} loading={loading} />
-      <ChatInput
-        addNewMessage={addNewMessage}
-        setJsonData={setJsonData}
-        setLoading={setLoading}
-        userId={userId}
-      />
+    <div className="relative bg-white flex flex-col pt-10 pb-4 rounded-lg space-y-3 my-4 mx-2 w-full h-full justify-between">
+      <div className="chatHistory overflow-auto px-10 pb-[160px]">
+        <MessageList list={message} loading={loading} />
+      </div>
+      <div className="absolute bottom-0 z-10 bg-white my-4 w-full px-10 py-4">
+        <ChatInput
+          addNewMessage={addNewMessage}
+          setJsonData={setJsonData}
+          setLoading={setLoading}
+          userId={userId}
+        />
+      </div>
     </div>
   );
 };
