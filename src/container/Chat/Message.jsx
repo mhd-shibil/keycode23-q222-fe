4fcd2fc3a0b90833beb5382e4a87fc16c28a5a2c
shibil <shibil@keyvalue.systems>
2023-09-15 22:32:40 +0530

    merge commit

--- src/container/Chat/Message.jsx
+++ src/container/Chat/Message.jsx
@@ -1,17 +1,26 @@
 const Message = ({ chatMsg, isUser }) => {
   return (
-    <div>
-      <div
-        className={`text-sm p-3 my-3 ${
-          isUser
-            ? "bg-blue-800 text-zinc-100  rounded-bl-3xl rounded-tl-3xl rounded-tr-3xl"
-            : "bg-slate-200 text-stone-950  rounded-bl-3xl rounded-br-3xl rounded-tr-3xl"
-        }
+    <>
+      <div className="w-full justify-end flex">
+        <div
+          className={`text-sm p-3 my-3 w-fit ${
+            isUser
+              ? "bg-blue-800 text-zinc-100 text-lg font-semibold rounded-bl-3xl rounded-tl-3xl rounded-tr-3xl"
+              : "bg-slate-200 text-stone-950  rounded-bl-3xl rounded-br-3xl rounded-tr-3xl"
+          }
         `}
-      >
-        {chatMsg}
+        >
+          {chatMsg}
+        </div>
       </div>
-    </div>
+      <div className="w-full justify-start flex">
+        <div
+          className={`text-sm p-3 my-3 w-fit font-semibold bg-slate-200 text-stone-950 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl`}
+        >
+          {chatMsg}
+        </div>
+      </div>
+    </>
   );
 };
 export default Message;
