ebd84ec982a75913926c1a2ded08193a1e2a099a
shibil <shibil@keyvalue.systems>
2023-09-15 22:59:47 +0530

    full screen video player

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -5,14 +5,14 @@ const AppWrap = (Component, idName, classNames) =>
   function HOC() {
     return (
       <div id={idName} className={`app__container ${classNames}`}>
-        <SocialMedia />
+        {/* <SocialMedia /> */}
         <div className="app__wrapper app__flex">
           <Component />
 
-          <div className="copyright">
+          {/* <div className="copyright">
             <p className="p-text">@2023 HACKATHON</p>
             <p className="p-text">All rights reserved</p>
-          </div>
+          </div> */}
         </div>
         <NavigationDots active={idName} />
       </div>
