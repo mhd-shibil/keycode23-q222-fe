e3a07b15c82cf2ead02056a90985dfa9b9c201f3
anandukeyvalue <anandu@keyvalue.systems>
2023-09-15 22:51:57 +0530

    loader added for the chat screen

--- src/container/Chat/Message.jsx
+++ src/container/Chat/Message.jsx
@@ -1,16 +1,37 @@
-const Message = ({ chatMsg, isUser }) => {
+import { Comment } from "react-loader-spinner";
+const Message = ({ chatMsg, isLast, loading }) => {
+  // "bg-slate-200 text-stone-950  rounded-bl-3xl rounded-br-3xl rounded-tr-3xl"
   return (
-    <div>
-      <div
-        className={`text-sm p-3 my-3 ${
-          isUser
-            ? "bg-blue-800 text-zinc-100  rounded-bl-3xl rounded-tl-3xl rounded-tr-3xl"
-            : "bg-slate-200 text-stone-950  rounded-bl-3xl rounded-br-3xl rounded-tr-3xl"
-        }
+    <div className="flex flex-col space-y-4">
+      <div className="w-full flex justify-end ">
+        <div
+          className={`text-sm p-3 my-3 w-fit bg-blue-800 text-zinc-100  rounded-bl-3xl rounded-tl-3xl rounded-tr-3xl
         `}
-      >
-        {chatMsg}
+        >
+          {chatMsg}
+        </div>
       </div>
+      {isLast && loading ? (
+        <Comment
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="comment-loading"
+          wrapperStyle={{}}
+          wrapperClass="comment-wrapper"
+          color="#000000"
+          backgroundColor="#FFFFFF"
+        />
+      ) : (
+        <div className="w-full flex justify-start ">
+          <div
+            className={`bg-slate-200 text-sm p-3 my-3 w-fit text-stone-950  rounded-bl-3xl rounded-br-3xl rounded-tr-3xl
+        `}
+          >
+            {"Done"}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/container/Chat/MessageList.jsx
+++ src/container/Chat/MessageList.jsx
@@ -1,10 +1,18 @@
 import Message from "./Message";
-<Message chatMsg={"hi......."} isUser={true} />;
-const MessageList = ({ list }) => {
+const MessageList = ({ list, loading }) => {
   return (
     <div className="space-y-6">
-      {list.map((msg) => {
-        return <Message chatMsg={msg} isUser={true} />;
+      {list.map((msg, index) => {
+        let isLast = false;
+        if (index === list.length - 1) isLast = true;
+        return (
+          <Message
+            chatMsg={msg}
+            isUser={true}
+            isLast={isLast}
+            loading={loading}
+          />
+        );
       })}
     </div>
   );
